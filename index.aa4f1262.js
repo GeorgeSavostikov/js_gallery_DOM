7561a442b8ff2e33c5ea1d81fb5cd759aa75237a
Heorhii Savostikov <georgesavostikov@gmail.com>
2024-07-04 23:07:22 +0300

    make build

---
+++ index.aa4f1262.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("ul"),t=document.querySelector("div > img");e.addEventListener("click",function(e){e.preventDefault();var r=e.target.closest("a");r&&t.setAttribute("src",r.href)});
+//# sourceMappingURL=index.aa4f1262.js.map
